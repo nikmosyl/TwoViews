c58702f30ee7000fa96376295bfec0ea77ff57f2
ns.moskalev <nikmosyl@gmail.com>
2023-11-01 20:52:53 +0400

    Initial Commit

---
+++ TwoViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViews
+//
+//  Created by nikita on 01.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
